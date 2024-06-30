cee2320c12fc5d4cf57c01fbb097c6fda9d41f77
mw08081 <president304@naver.com>
2024-06-30 18:03:28 +0900

    Make new repo for rpm(testing)

---
+++ test_led.py
@@ -0,0 +1,16 @@
+
+import RPi.GPIO as gp
+import time
+
+pins = [16,20,21]
+gp.setmode(gp.BCM)
+
+for pin in pins :
+    gp.setup(pin, gp.OUT)
+
+for pin in pins :
+    print(pin, type(pin))
+    gp.output(pin, True)
+input()
+for pin in pins :
+    gp.output(pin, False)
